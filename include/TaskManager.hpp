b41cc671e63a30854758538f565fea3a89ef4509
Dawid Jakubowski <selfashynook@gmail.com>
2026-08-08 12:59:11 +0200

    Expose ranked fuzzy task search

--- include/TaskManager.hpp
+++ include/TaskManager.hpp
@@ -1,6 +1,31 @@
 #pragma once
+
 #include "Task.hpp"
+
 #include <cstddef>
+#include <optional>
 #include <string>
 #include <vector>
-class TaskManager { public: Task& AddTask(std::string name); bool RemoveTask(std::size_t index); Task* GetTask(std::size_t index); const Task* GetTask(std::size_t index) const; std::vector<std::size_t> Search(const std::string& query) const; const std::vector<Task>& GetTasks() const noexcept; private: std::vector<Task> tasks_; };
+
+struct TaskSearchResult {
+    std::size_t index;
+    int score;
+};
+
+class TaskManager {
+public:
+    Task& AddTask(std::string name);
+    bool RemoveTask(std::size_t index);
+
+    Task* GetTask(std::size_t index);
+    const Task* GetTask(std::size_t index) const;
+
+    std::vector<TaskSearchResult> FuzzySearchTasks(const std::string& query) const;
+    std::vector<std::size_t> Search(const std::string& query) const;
+    std::optional<std::size_t> FindBestMatch(const std::string& query) const;
+
+    const std::vector<Task>& GetTasks() const noexcept;
+
+private:
+    std::vector<Task> tasks_;
+};
