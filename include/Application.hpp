17fb2b97c9694e34b66c451fa07ce9e9850df8f2
Dawid Jakubowski <selfashynook@gmail.com>
2026-08-09 10:37:42 +0200

    Add interactive fuzzy command palette

--- include/Application.hpp
+++ include/Application.hpp
@@ -14,17 +14,31 @@ public:
     int Run();
 
 private:
+    struct Suggestion {
+        std::string label;
+        std::string replacement;
+    };
+
     void HandleInput();
+    void HandleMouse();
+    void AcceptSuggestion(const std::vector<Suggestion>& suggestions);
+    void ResetSuggestionSelection();
+
     void Render();
     void RenderHeader();
     void RenderTasks();
+    void RenderSuggestions(const std::vector<Suggestion>& suggestions);
     void RenderStatus();
     void RenderCommandLine();
 
+    std::vector<Suggestion> BuildSuggestions() const;
     std::vector<std::size_t> VisibleTaskIndices() const;
+    int SuggestionStartRow(std::size_t suggestionCount) const;
+
     static std::string FormatDuration(std::chrono::seconds duration);
 
     TaskManager taskManager_;
     CommandProcessor commandProcessor_;
     std::string commandBuffer_;
+    std::size_t selectedSuggestion_{0};
 };
