c9afdc013cbb3bd167593eb790497104e7581507
Dawid Jakubowski <selfashynook@gmail.com>
2026-08-09 21:15:09 +0200

    Expose task creation history and time fragments in stats

--- SentinelStats/include/StatisticsData.hpp
+++ SentinelStats/include/StatisticsData.hpp
@@ -16,12 +16,30 @@ struct ProjectSeries {
     std::vector<TimePointValue> locHistory;
 };
 
+struct TaskFragment {
+    std::int64_t startedAtEpoch{0};
+    std::int64_t endedAtEpoch{0};
+    std::uint64_t durationSeconds{0};
+    bool open{false};
+};
+
+struct TaskFragmentSeries {
+    std::string id;
+    std::string name;
+    std::string source;
+    std::int64_t createdAtEpoch{0};
+    std::vector<TaskFragment> fragments;
+};
+
 struct StatisticsSnapshot {
     std::size_t totalTasks{0};
     std::size_t completedTasks{0};
     std::size_t runningTasks{0};
+    std::size_t totalFragments{0};
     std::uint64_t totalTrackedSeconds{0};
+    std::vector<TimePointValue> createdTaskHistory;
     std::vector<TimePointValue> completedTaskHistory;
+    std::vector<TaskFragmentSeries> taskFragments;
     std::vector<ProjectSeries> projects;
 };
 
