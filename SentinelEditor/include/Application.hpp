4bec97b89a28e2c419c6fe37917984f38fc1abea
Dawid Jakubowski <selfashynook@gmail.com>
2026-08-10 14:27:41 +0200

    Rework SentinelEditor application interface for GTK

--- SentinelEditor/include/Application.hpp
+++ SentinelEditor/include/Application.hpp
@@ -3,7 +3,9 @@
 #include "EditorBuffer.hpp"
 #include "TypingMetrics.hpp"
 
-#include <cstddef>
+#include <gtk/gtk.h>
+
+#include <filesystem>
 #include <string>
 
 class Application {
@@ -18,62 +20,83 @@ private:
         Search
     };
 
-    void HandleInput(int key);
-    void HandleNormalInput(int key);
-    void HandleInsertInput(int key);
-    void HandleCommandInput(int key);
-    void HandleSearchInput(int key);
-    void HandleMouse();
+    static void OnActivate(GtkApplication* app, gpointer userData);
+    static gboolean OnTextKeyPress(GtkWidget* widget, GdkEventKey* event, gpointer userData);
+    static gboolean OnCommandEntryKeyPress(GtkWidget* widget, GdkEventKey* event, gpointer userData);
+    static void OnBufferChanged(GtkTextBuffer* buffer, gpointer userData);
+    static void OnOpenClicked(GtkButton* button, gpointer userData);
+    static void OnSaveClicked(GtkButton* button, gpointer userData);
+    static void OnSaveAsClicked(GtkButton* button, gpointer userData);
+    static void OnNewClicked(GtkButton* button, gpointer userData);
+    static gboolean OnWindowDelete(GtkWidget* widget, GdkEvent* event, gpointer userData);
+    static gboolean OnRefreshTimer(gpointer userData);
 
-    void EnterNormalMode();
-    void EnterInsertMode();
-    void EnterCommandMode();
-    void EnterSearchMode();
+    void BuildWindow(GtkApplication* app);
 
+    gboolean HandleTextKeyPress(GdkEventKey* event);
+    gboolean HandleNormalKey(GdkEventKey* event);
+    gboolean HandleInsertKey(GdkEventKey* event);
+    gboolean HandleCommandEntryKey(GdkEventKey* event);
+
+    void EnterMode(Mode mode);
     void ExecuteCommand(const std::string& command);
-    bool LoadFile(const std::string& path, bool force);
-    bool SaveFile(const std::string& path = {});
 
-    void MoveHorizontal(int delta);
-    void MoveVertical(int delta);
+    bool LoadFile(const std::filesystem::path& path, bool force);
+    bool SaveFile(const std::filesystem::path& path = {});
+    void NewBuffer(bool force);
+    void OpenFileDialog();
+    bool SaveFileDialog();
+    bool ConfirmDiscardOrSave();
+
+    void SyncGtkFromEditorBuffer();
+    void SyncEditorBufferFromGtk();
+
+    GtkTextIter CursorIter() const;
+    void PlaceCursor(GtkTextIter iter, bool scroll = true);
+    void MoveHorizontal(int direction);
+    void MoveVertical(int direction);
     void MoveWordForward();
     void MoveWordBackward();
-    void MovePage(int direction);
-    void ClampCursor();
-    void EnsureCursorVisible();
+    void MoveLineStart();
+    void MoveLineEnd(bool insertionPoint);
+    void MoveDocumentStart();
+    void MoveDocumentEnd();
+    void DeleteCharacter();
+    void DeleteLine();
+    void OpenLineBelow();
+    void OpenLineAbove();
 
     bool FindNext(const std::string& query, bool wrap);
 
-    void Render();
-    void RenderBuffer(int height, int width);
-    void RenderStatusLine(int row, int width);
-    void RenderCommandLine(int row, int width);
-
-    std::size_t GutterWidth() const;
-    std::size_t TextRows() const;
-    std::size_t TextColumns() const;
+    void UpdateStatusBar();
+    void UpdateWindowTitle();
+    void SetStatus(std::string status);
     std::string ModeName() const;
 
     static std::string Trim(std::string value);
     static std::string Unquote(std::string value);
-    static std::string ExpandTabs(const std::string& value, std::size_t tabWidth = 4);
 
-    EditorBuffer buffer_;
+    GtkApplication* app_{nullptr};
+    GtkWidget* window_{nullptr};
+    GtkWidget* textView_{nullptr};
+    GtkTextBuffer* textBuffer_{nullptr};
+    GtkWidget* statusLabel_{nullptr};
+    GtkWidget* metricsLabel_{nullptr};
+    GtkWidget* commandBox_{nullptr};
+    GtkWidget* commandPromptLabel_{nullptr};
+    GtkWidget* commandEntry_{nullptr};
+
+    EditorBuffer editorBuffer_;
     TypingMetrics typingMetrics_;
+
     Mode mode_{Mode::Normal};
-
-    std::size_t cursorRow_{0};
-    std::size_t cursorColumn_{0};
-    std::size_t preferredColumn_{0};
-    std::size_t topLine_{0};
-    std::size_t leftColumn_{0};
-
-    bool running_{true};
     bool pendingDelete_{false};
     bool pendingGoto_{false};
+    bool suppressBufferChanged_{false};
 
-    std::string commandBuffer_;
-    std::string searchBuffer_;
+    std::filesystem::path initialPath_;
     std::string lastSearch_;
     std::string status_;
+
+    guint refreshTimerId_{0};
 };
